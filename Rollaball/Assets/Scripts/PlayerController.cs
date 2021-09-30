4b8608a16a4f50f239c3db1933b037017d8ac232
g-rude <77759910+g-rude@users.noreply.github.com>
2021-09-30 17:55:33 -0400

    fixed code to allow movement
    also changed camera positioning

--- Rollaball/Assets/Scripts/PlayerController.cs
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -7,29 +7,40 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
 
+    public float speed = 0;
+
     private Rigidbody rb;
+    
+
     private float movementX;
     private float movementY;
+
 
     // Start is called before the first frame update
     void Start()
     {
+       // this is not strictly enforced
         rb = GetComponent<Rigidbody>();
+        
     }
 
-    void OnMove(InputValue movementValue)
+    private void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
 
+
+        // this is a no no
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
 
-    void FixedUpdate()
+    // Update is called once per frame
+    private void FixedUpdate()
     {
+        //this is messy
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
-        rb.AddForce(movement);
+        rb.AddForce(movement * speed);
     }
 
 }
