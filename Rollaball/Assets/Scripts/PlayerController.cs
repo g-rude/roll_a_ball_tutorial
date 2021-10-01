21cbb52197dea867af70070d4334e7a2f9835bf1
g-rude <77759910+g-rude@users.noreply.github.com>
2021-09-30 23:41:32 -0400

    added if statement for tag comparison

--- Rollaball/Assets/Scripts/PlayerController.cs
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -45,7 +45,13 @@ public class PlayerController : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        other.gameObject.SetActive(false);
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+       
     }
+
+
 
 }
